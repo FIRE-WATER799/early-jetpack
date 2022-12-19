37660747335bbe433256a1855d34c48f7ffbf836
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2022-12-19 13:22:47 -0600

    This should work?

---
+++ data-updates.lua
@@ -0,0 +1,7 @@
+data.raw["technology"]["jetpack-1"].unit.ingredients = {
+    {"automation-science-pack", 1},
+    {"logistic-science-pack", 1}
+}
+data.raw["technology"]["jetpack-1"].prerequisites = {
+    "solar-panel-equipment",
+}
